dd82a2904a346b6ef4bdd412f12038601c70c38a
THANH <le627959@gmail.com>
2026-09-22 22:10:32 +0700

    typedef structure in main.c

--- Src/main.c
+++ Src/main.c
@@ -1,7 +1,28 @@
-#define F_CPU 8000000
 #include <avr/io.h>
 #include <util/delay.h>
+
+typedef struct
+{
+    enum Status {
+        ON,
+        OFF
+    }Status;
+    enum Power_Lever {
+        LOW,
+        MEDIUM,
+        HIGH,
+    }Power_Lever;
+}Device;
+
+Device *LED,*WAVE;
+
+
+
+
 int main(void){
+    LED->Status = OFF;
+    WAVE->Status = OFF;
+
     while(1){
         PORTB = 0xFF;
         _delay_ms(1000);
